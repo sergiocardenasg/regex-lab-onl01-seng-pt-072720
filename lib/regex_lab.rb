d856fb6fad8bed97e8e28e6ab467a88f249ba59d
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-08 00:21:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.match(/\A[aeiou]/)
+  word.match(/\^[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.
 end
 
 def valid_phone_number?(phone)
